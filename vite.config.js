698a8070ec871a3093b77cf7292a0429cb1fcc4c
DiogoSousa <vdiogov@gmail.com>
2022-10-09 12:36:59 -0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Atlantis-Frontend020931/',
+  base: '/Atlantis-Frontend20931/',
   plugins: [vue()],
   resolve: {
     alias: {
